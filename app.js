71dff28a9ba87f1fa4ce6a2114e39beab99bf82c
ShivaKanchi <shivakanchi111@gmail.com>
2022-10-02 21:46:28 +0530

    Started with backend

--- app.js
+++ app.js
@@ -3,4 +3,13 @@ const express = require('express');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const errorhandlers = require("./handlers/errorHandler");
+app.use(errorhandlers.notFound);
+app.use(errorhandlers.mongoseErrors);
+if (process.env.ENV === "Development") {
+    app.use(errorhandlers.developmentErrors);
+} else {
+    app.use(errorhandlers.productionErrors);
+}
+
 module.exports = app;
