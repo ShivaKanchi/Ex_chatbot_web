764b5f48e7a713d4937e7f75c916ef769a5eee45
ShivaKanchi <shivakanchi111@gmail.com>
2022-10-15 20:24:37 +0530

    Starting frontend

--- ex_chatbot_frontend/src/App.js
+++ ex_chatbot_frontend/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import dashboard from "./pages/dashboard";
+import login from "./pages/login";
+import register from "./pages/register";
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <BrowserRouter>
+    <switch>
+      <Route path="/register" component={register} />
+      <Route path="/login" component={login} />
+      <Route path="/dashboard" component={dashboard} />
+    </switch>
+  </BrowserRouter>
 }
 
 export default App;
