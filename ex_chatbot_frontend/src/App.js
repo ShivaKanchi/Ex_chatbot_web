f0399bc5bd3d73e6b2ca17c4396ab9b427f2ad61
ShivaKanchi <shivakanchi111@gmail.com>
2022-10-15 20:57:13 +0530

    Created routes and pages

--- ex_chatbot_frontend/src/App.js
+++ ex_chatbot_frontend/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import dashboard from "./pages/dashboard";
 import login from "./pages/login";
 import register from "./pages/register";
 function App() {
-  return (<BrowserRouter>
-    <switch>
-      <Route path="/register" component={register} />
-      <Route path="/login" component={login} />
-      <Route path="/dashboard" component={dashboard} />
-    </switch>
-  </BrowserRouter>);
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/register" component={register} />
+        <Route path="/login" component={login} />
+        <Route path="/dashboard" component={dashboard} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
